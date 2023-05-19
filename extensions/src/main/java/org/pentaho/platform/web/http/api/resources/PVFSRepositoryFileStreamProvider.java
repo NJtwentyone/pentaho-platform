bbed689924fb705dbd44335adf4973233d955fb8
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2023-05-19 18:58:18 -0400

    [WIP] able to write to AWS S3 - hard setting ConnectionManger to use XmlMetaStore, PUC will stop working after first call to scheduler - calling PVFSRepositoryFileStreamProvider#setXmlMetaStore right before calling KettleVFS.getOutputStream()

--- extensions/src/main/java/org/pentaho/platform/web/http/api/resources/PVFSRepositoryFileStreamProvider.java
+++ extensions/src/main/java/org/pentaho/platform/web/http/api/resources/PVFSRepositoryFileStreamProvider.java
@@ -22,6 +22,13 @@ package org.pentaho.platform.web.http.api.resources;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.pentaho.di.connections.ConnectionManager;
+import org.pentaho.di.core.vfs.KettleVFS;
+import org.pentaho.di.metastore.MetaStoreConst;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.metastore.api.IMetaStore;
+import org.pentaho.metastore.api.exceptions.MetaStoreException;
 import org.pentaho.platform.api.action.IStreamingAction;
 import org.pentaho.platform.api.repository2.unified.IUnifiedRepository;
 import org.pentaho.platform.api.repository2.unified.RepositoryFile;
@@ -33,6 +40,7 @@ import org.pentaho.platform.repository2.unified.jcr.PentahoJcrConstants;
 import org.pentaho.platform.util.web.MimeHelper;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
@@ -49,6 +57,8 @@ public class PVFSRepositoryFileStreamProvider extends RepositoryFileStreamProvid
   private static final long serialVersionUID  = 2123027232821453048L; // POC changed UID from //2812310908328498989L;
 
   private static final Log logger = LogFactory.getLog( PVFSRepositoryFileStreamProvider.class );
+
+  IMetaStore saveMetaStore;
 
 //  public String outputFilePath;
 //  public String inputFilePath;
@@ -100,11 +110,50 @@ public class PVFSRepositoryFileStreamProvider extends RepositoryFileStreamProvid
       }
     }
 
+    String filename = tempOutputFilePath.substring( tempOutputFilePath.lastIndexOf( RepositoryFile.SEPARATOR ) + 1 );
+    String fullPVFSFilePath = concat( getPVFSOutputDirectory() , filename);
+
+    /**
+     * Milestone: able create a file on AWS S3, however the file is empty
+     *
+     * NOTE:
+     * after this is called, the PUC application will stop working, PUC expectes the IMetaStore to Be PurRepositoryMetaStore.java
+     * TODO look into importing the providers from XmlMetaStore into PurRepositoryMetaStore
+     * TODO look into how the default providers are created for PurRepositoryMetaStore
+     * */
+
+    setXmlMetaStore();
+
+    OutputStream pvfsOutputStream = KettleVFS.getOutputStream( fullPVFSFilePath, false);
+
     RepositoryFileOutputStream outputStream =
         new RepositoryFileOutputStream( tempOutputFilePath, autoCreateUniqueFilename(), true );
     outputStream.addListener( this );
     outputStream.forceFlush( false );
-    return outputStream;
+
+    return usePVFS() ? pvfsOutputStream : outputStream;
+  }
+
+  public boolean usePVFS() {
+    return System.getenv().containsKey( "POC_PVFS_ENABLE"  );
+  }
+
+  public String getPVFSOutputDirectory() {
+    return System.getenv().getOrDefault( "POC_PVFS_OUTPUT_DIR", null );
+  }
+
+  public String concat(String replacementPath, String filename ){
+    return replacementPath + "/" + filename;
+  }
+
+  public void setXmlMetaStore() {
+    ConnectionManager.getInstance().setMetastoreSupplier( () -> {
+      try {
+        return MetaStoreConst.openLocalPentahoMetaStore(); // POC should be XmlMetaStore ie  ~/.pentaho/metastore/pentaho/
+      } catch ( MetaStoreException e ) {
+        throw new IllegalStateException( "could not creat XmlMetaStore object", e);
+      }
+    });
   }
 
   public String toString() {
